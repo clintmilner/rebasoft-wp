5dd93861c65bd4f7054efe40aea7be1ecd4c3f9e
Clint Milner <clinton.milner@gmail.com>
2015-01-30 11:43:34 +0000

    add GA code to footer

--- wp-content/themes/interface/footer.php
+++ wp-content/themes/interface/footer.php
@@ -74,4 +74,16 @@
 		do_action( 'interface_after' );
 	?>
 <?php wp_footer(); ?>
+
+<script type="text/javascript">
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-34829205-1']);
+	_gaq.push(['_trackPageview']);
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+</script>
+
 </body></html>
